3df21d2eb79ab059d3d46566320d119ce50a4cab
pavlinapospi <144594248+pavlinapospi@users.noreply.github.com>
2024-11-12 10:43:37 +0800

    Update url.php

--- www/assets/url.php
+++ www/assets/url.php
@@ -15,8 +15,5 @@ function redirectUrl($path) {
         $url_protocol = "http";
     }
 
-    //localhost = $_SERVER["HTTP_HOST"]
-    //header("location: one-staff.php?id=$id");
-    
     header("location: $url_protocol://" .$_SERVER ["HTTP_HOST"] . $path);
 }
